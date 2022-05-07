aeaa0b4363703454afc7e04afa18e68aebdd94a6
WuCian <igorjuraszek@gmail.com>
2022-05-07 15:41:45 +0200

    adding-homework

--- app/user/show/route.js
+++ app/user/show/route.js
@@ -4,8 +4,8 @@ import { inject as service } from '@ember/service';
 export default class UserShowRoute extends Route {
   @service store;
 
-  async model(params) {
-    const user = await this.store.findRecord('user', params.id);
-    return user;
+  // destrukturyzacja parametrów
+  async model({ id }) {
+    return await this.store.findRecord('user', id);
   }
 }
