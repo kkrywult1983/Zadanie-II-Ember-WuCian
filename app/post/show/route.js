65dd5c123fd5eaece6d936a685ce202f751361f9
WuCian <igorjuraszek@gmail.com>
2022-05-07 15:24:38 +0200

    adding-post-view

--- app/post/show/route.js
+++ app/post/show/route.js
@@ -1,3 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class PostShowRoute extends Route {}
+export default class PostShowRoute extends Route {
+  @service store;
+
+  async model(params) {
+    const post = await this.store.findRecord('post', params.id);
+    return post;
+  }
+}
