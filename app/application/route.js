5b189640f156609d8c525303368587a070071fa2
WuCian <igorjuraszek@gmail.com>
2022-05-07 13:43:46 +0200

    updating-post-owner

--- app/application/route.js
+++ app/application/route.js
@@ -19,27 +19,31 @@ export default class ApplicationRoute extends Route {
       email: 'user@user.com',
     };
 
+    const user1Model = this.store.createRecord('user', user1);
+    const user2Model = this.store.createRecord('user', user2);
+    await user1Model.save();
+    await user2Model.save();
+
     const post1 = {
       title: 'Testowy 1',
       body: 'Zawartość testowa 1',
+      owner: user1Model,
     };
     const post2 = {
       title: 'Testowy 2',
       body: 'Zawartość testowa 2',
+      owner: user1Model,
     };
     const post3 = {
       title: 'Testowy 3',
       body: 'Zawartość testowa 3',
+      owner: user2Model,
     };
 
-    const user1Model = this.store.createRecord('user', user1);
-    const user2Model = this.store.createRecord('user', user2);
     const post1Model = this.store.createRecord('post', post1);
     const post2Model = this.store.createRecord('post', post2);
     const post3Model = this.store.createRecord('post', post3);
 
-    await user1Model.save();
-    await user2Model.save();
     await post1Model.save();
     await post2Model.save();
     await post3Model.save();
--- app/post/model.js
+++ app/post/model.js
@@ -1,8 +1,8 @@
-import Model, { attr } from '@ember-data/model';
+import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class PostModel extends Model {
-  @attr('string', { defaultValue: 'Unknown User' }) owner;
   @attr('string') title;
   @attr('string') body;
   @attr('boolean', { defaultValue: false }) isDeleted;
+  @belongsTo('user') owner;
 }
