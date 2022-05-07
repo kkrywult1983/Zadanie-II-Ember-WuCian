af352bacf16518226facc7765d5bbda9c28708c5
WuCian <igorjuraszek@gmail.com>
2022-05-07 12:48:34 +0200

    adding-model-post-and-updating-show-posts

--- app/application/route.js
+++ app/application/route.js
@@ -6,6 +6,7 @@ export default class ApplicationRoute extends Route {
 
   async beforeModel() {
     window.localStorage.clear();
+
     const user1 = {
       username: 'admin',
       password: 'admin123',
@@ -18,9 +19,29 @@ export default class ApplicationRoute extends Route {
       email: 'user@user.com',
     };
 
+    const post1 = {
+      title: 'Testowy 1',
+      body: 'Zawartość testowa 1',
+    };
+    const post2 = {
+      title: 'Testowy 2',
+      body: 'Zawartość testowa 2',
+    };
+    const post3 = {
+      title: 'Testowy 3',
+      body: 'Zawartość testowa 3',
+    };
+
     const user1Model = this.store.createRecord('user', user1);
     const user2Model = this.store.createRecord('user', user2);
+    const post1Model = this.store.createRecord('post', post1);
+    const post2Model = this.store.createRecord('post', post2);
+    const post3Model = this.store.createRecord('post', post3);
+
     await user1Model.save();
     await user2Model.save();
+    await post1Model.save();
+    await post2Model.save();
+    await post3Model.save();
   }
 }
