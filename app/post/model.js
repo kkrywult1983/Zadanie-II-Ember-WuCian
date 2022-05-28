d26c3694a870d7b1714900db93477abac11c7ae1
WuCian <igorjuraszek@gmail.com>
2022-05-28 13:50:39 +0200

    Adding like model

--- app/post/model.js
+++ app/post/model.js
@@ -1,8 +1,9 @@
-import Model, { attr, belongsTo } from '@ember-data/model';
+import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 
 export default class PostModel extends Model {
   @attr('string') title;
   @attr('string') body;
   @attr('boolean', { defaultValue: false }) isDeleted;
   @belongsTo('user') owner;
+  @hasMany('like') likes;
 }
