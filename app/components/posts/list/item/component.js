8bcadb11ecdf3ac84e466023e4008e4e9e367105
Igor Juraszek <igorjuraszek@gmail.com>
2022-06-04 10:50:01 +0200

    Adding likes and dislikes feature

--- app/components/posts/list/item/component.js
+++ app/components/posts/list/item/component.js
@@ -1,14 +1,42 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class PostsListItemComponent extends Component {
   @service session;
+  @service store;
+
+  get currentUser() {
+    return this.session.currentUser;
+  }
+
+  get currentPost() {
+    return this.args.post;
+  }
+
+  @action
+  async onLike() {
+    const createdLike = this.store.createRecord('like', {
+      user: this.currentUser,
+      post: this.currentPost,
+    });
+
+    await createdLike.save();
+  }
+
+  @action
+  async onDislike() {
+    console.log('Dislike');
+    const like = this.currentUser.likes.find((like) => {
+      return like.post.get('id') === this.currentPost.id;
+    });
+
+    await like.destroyRecord();
+  }
 
   get isAlreadyLikedByCurrentUser() {
-    const { currentUser } = this.session;
-    const { post } = this.args;
-    const likeModel = currentUser.likes.find((like) => {
-      return like.post.get('id') === post.id;
+    const likeModel = this.currentUser.likes.find((like) => {
+      return like.post.get('id') === this.currentPost.id;
     });
     const isLikeExists = Boolean(likeModel);
     return isLikeExists;
