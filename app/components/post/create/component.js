9c8728bdf4cd1bb46a19c0e4a07cdea32b829ba6
Igor Juraszek <igorjuraszek@gmail.com>
2022-06-04 15:42:50 +0200

    Improoved modal functions

--- app/components/post/create/component.js
+++ app/components/post/create/component.js
@@ -1,9 +1,18 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class PostCreateModalComponent extends Component {
+  @service session;
+  @service store;
   @tracked isShowCreateModal = false;
+  @tracked title = '';
+  @tracked content = '';
+
+  get hasEmptyFields() {
+    return !(this.title && this.content);
+  }
 
   @action
   onShowModal() {
@@ -13,6 +22,35 @@ export default class PostCreateModalComponent extends Component {
 
   @action onHideModal() {
     console.log('Hide modal');
+    this.clearFields();
     this.isShowCreateModal = false;
+  }
+
+  @action
+  onTitleChange(event) {
+    this.title = event.target.value;
+  }
+
+  @action
+  onContentChange(event) {
+    this.content = event.target.value;
+  }
+
+  @action
+  async onSave() {
+    const post = {
+      title: this.title,
+      body: this.content,
+      owner: this.session.currentUser,
+    };
+    const postModel = this.store.createRecord('post', post);
+    await postModel.save();
+    this.clearFields();
+    this.isShowCreateModal = false;
+  }
+
+  clearFields() {
+    this.title = '';
+    this.content = '';
   }
 }
